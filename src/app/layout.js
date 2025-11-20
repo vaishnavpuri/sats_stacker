fb37aca3025c9a08051d772d7a025a509e211d18
Vaishnav Puri <56321564+vaishnavpuri@users.noreply.github.com>
2025-11-20 02:39:52 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "SatoshiSignal",
-  description: "The Reactive Accumulation Engine",
+  description: "The Reactive BTC Accumulation Engine",
 };
 
 export default function RootLayout({ children }) {
